989e14e2f329fa84f25ad610cb3183e0d2f8aff8
Evgeniy Golovin <evgeniy.golovin@unitpoint.ru>
2014-09-11 04:18:49 +0400

    split ox-binder: .h & .cpp

--- examples/ElasticTransitionOS/proj.android/jni/src/Android.mk
+++ examples/ElasticTransitionOS/proj.android/jni/src/Android.mk
@@ -6,12 +6,15 @@ LOCAL_MODULE := main
 #SDK_ROOT points to folder with SDL and oxygine-framework
 LOCAL_SRC_FILES := ../../../../../SDL/src/main/android/SDL_android_main.c
 
-LOCAL_SRC_FILES += ../../../src/example.cpp ../../../src/entry_point.cpp 
-
 LOCAL_SRC_FILES += ../../../../../objectscript/src/objectscript.cpp
 LOCAL_SRC_FILES += ../../../../../objectscript/src/os-heap.cpp
 LOCAL_SRC_FILES += ../../../../../objectscript/src/ext-datetime/os-datetime.cpp
 LOCAL_SRC_FILES += ../../../../../objectscript/src/EaseFunction.cpp
+LOCAL_SRC_FILES += ../../../../../objectscript/src/ox-binder.cpp
+
+LOCAL_SRC_FILES += ../../../src/example.cpp ../../../src/entry_point.cpp 
+
+
 
 LOCAL_STATIC_LIBRARIES := oxygine-framework_static
 LOCAL_SHARED_LIBRARIES := SDL2
