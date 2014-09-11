17029dbb9b6b09628bdbd5263b069d6a08efb1a2
Evgeniy Golovin <evgeniy.golovin@unitpoint.ru>
2014-09-11 04:10:28 +0400

    setup templates

--- oxygine-framework/tools/templates/proj.android/install.bat
+++ oxygine-framework/tools/templates/proj.android/install.bat
@@ -1,2 +1,2 @@
 adb install -r bin/${PROJECT}-debug.apk
-adb shell am start -n org.oxygine.${PROJECT}/org.oxygine.${PROJECT}.MainActivity
+adb shell am start -n org.os2d.${PROJECT}/org.os2d.${PROJECT}.MainActivity
