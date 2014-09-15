eab355ecefe6e210788c7190d2d7630753580b18
Evgeniy Golovin <evgeniy.golovin@unitpoint.ru>
2014-09-16 00:14:43 +0400

    remove builds of not yet tested platforms

--- examples/SeqTweenOS/proj.emscripten/build_release.bat
+++
@@ -1,7 +0,0 @@
-python ../../..//tools/others/embed_folder_js.py -s ../data
-
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN%/cmake/platform/emscripten.cmake -G"Unix Makefiles" .. 
-make
-cd ..
